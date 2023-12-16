a698370000d1243357ca1b121b9f88e6c6c045fd
Yab1 <yabilisanu@gmail.com>
2023-12-16 22:55:00 +0300

    Add 'Playpen Sans' font to extend the font family in the Tailwind config for enhanced typography options. Introduce 'playpen' as a new font family with fallback to sans-serif. No known issues. No issue references.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        playpen: ["Playpen Sans", "sans-serif"],
+      },
+    },
+  },
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
