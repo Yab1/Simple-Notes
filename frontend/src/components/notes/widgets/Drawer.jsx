9030dc5feabbd77959aa64b3ed469f3cfe1e3edf
Yab1 <yabilisanu@gmail.com>
2023-12-17 12:00:44 +0300

    Refactor router and Drawer to utilize child routes and Outlet
    The changes refactor the router to utilize child routes and update
    Drawer component to use Outlet for rendering. This builds a more modular
    and maintainable structure for routing and widget rendering in the
    application. It also improves code organization and readability.
    
    #Issue 123

--- frontend/src/components/notes/widgets/Drawer.jsx
+++ frontend/src/components/notes/widgets/Drawer.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 
 function Drawer() {
   const tags = [
@@ -32,30 +33,7 @@ function Drawer() {
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content px-5 pt-10 h-4">
-        <div className="card w-1/2 bg-pink-100 shadow-xl">
-          <div className="card-body grid grid-cols-4">
-            <h2 className="card-title col-span-2">Financial Tips </h2>
-            <div className="card-actions justify-end col-start-4">
-              <button className="btn btn-sm btn-info">Assignment</button>
-            </div>
-
-            <p className="text-sm py-2 col-span-full">
-              Learn essential financial tips for managing your budget and
-              achieving your savings goals. Discover strategies to make the most
-              of your hard-earned money.
-            </p>
-            <p className="uppercase">09:35PM</p>
-            <p className="uppercase col-span-2 col-start-3 ml-auto">
-              07 January 2023
-            </p>
-          </div>
-        </div>
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
+        <Outlet />
       </div>
       <div className="drawer-side h-fit">
         <label
