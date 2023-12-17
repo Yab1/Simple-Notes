37a48bd2bbd86e309aa977ef6fde855ddf874885
Yab1 <yabilisanu@gmail.com>
2023-12-17 12:21:24 +0300

    Refactor ErrorPage layout to use React Router Links
    Replace anchor tag with React Router Link component to enable
    navigation. Enhances user experience. No associated issues.

--- frontend/src/layouts/ErrorPage.jsx
+++ frontend/src/layouts/ErrorPage.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function ErrorPage() {
   return (
     <div className="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0">
@@ -19,7 +21,7 @@ function ErrorPage() {
           <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"></path>
           </svg>
-          <span>Return Home</span>
+          <Link to={"notes"}>Return Home</Link>
         </a>
       </div>
       <div className="w-1/2 lg:h-full flex lg:items-end justify-center p-4">
