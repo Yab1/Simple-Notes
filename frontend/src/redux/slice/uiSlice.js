fa51d76803e5ce4f92d9b92a879291a83cda3714
Yab1 <yabilisanu@gmail.com>
2023-12-17 14:48:15 +0300

    Refactor initial state to include predefined tags
    Updated the initial state in uiSlice.js to include predefined tags for
    better organization and visualization of different tasks. This change
    aims to provide a more structured and user-friendly experience for
    managing tasks. No related issues.

--- frontend/src/redux/slice/uiSlice.js
+++ frontend/src/redux/slice/uiSlice.js
@@ -1,6 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {};
+const initialState = {
+  tags: [
+    { name: "Assignment", background: "bg-info", badge: "badge-info" },
+    { name: "Ideas", background: "bg-success", badge: "badge-success" },
+    { name: "Meetings", background: "bg-primary", badge: "badge-primary" },
+    { name: "Personal", background: "bg-secondary", badge: "badge-secondary" },
+    { name: "Project", background: "bg-warning", badge: "badge-warning" },
+    { name: "Shopping", background: "bg-info", badge: "badge-info" },
+    { name: "Study", background: "bg-error", badge: "badge-error" },
+    { name: "Wishlist", background: "bg-success", badge: "badge-success" },
+    { name: "Work", background: "bg-primary", badge: "badge-primary" },
+  ],
+};
 
 const uiSlice = createSlice({
   name: "ui",
