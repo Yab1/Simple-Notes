df02398f84fe200519aa941bd47e101a0b2b555b
Yab1 <yabilisanu@gmail.com>
2023-12-17 12:00:18 +0300

    Created a boilerplate

---
+++ frontend/src/components/notes/pages/index.js
@@ -0,0 +1,7 @@
+export { default as Notes } from "./Notes";
+export { default as CreateNote } from "./CreateNote";
+export { default as EditNote } from "./EditNote";
+export { default as Profile } from "./Profile";
+export { default as Settings } from "./Settings";
+export { default as About } from "./About";
+export { default as childRoutes } from "./routes";
