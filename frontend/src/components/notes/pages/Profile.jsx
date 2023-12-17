4a5b83a9e766aaf2716c59f39097455c182e2dfe
Yab1 <yabilisanu@gmail.com>
2023-12-17 17:50:50 +0300

    Refactor Profile page to include AvatarSelector component
    Introduce AvatarSelector component to enable profile picture selection,
    improving user experience and visual customization. Closes #1234

--- frontend/src/components/notes/pages/Profile.jsx
+++ frontend/src/components/notes/pages/Profile.jsx
@@ -1,5 +1,37 @@
+import { AvatarSelector } from "../widgets";
+
 function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div className="grid gap-5">
+      <form action="" className="grid grid-cols-5 grid-rows-2">
+        <div className="avatar row-span-2 col-span-1">
+          <div className="w-32 rounded">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Username"
+          className="input input-bordered w-full col-span-3"
+        />
+        <input
+          type="text"
+          placeholder="Password"
+          className="input input-bordered w-full row-start-2 col-span-3 col-start-2"
+        />
+
+        <button
+          type="submit"
+          className="btn btn-neutral row-span-2 w-fit place-self-center"
+        >
+          Save Changes
+        </button>
+      </form>
+
+      <AvatarSelector />
+    </div>
+  );
 }
 
 export default Profile;
