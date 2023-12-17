5d0a30735964a666ac84756101328dd84021fda4
Yab1 <yabilisanu@gmail.com>
2023-12-17 13:01:40 +0300

    Refactor CreateNote component to include form for note input
    Transformed the CreateNote component to integrate a form for users to
    input note details and select a category. This improves user experience
    and allows for more structured note creation. Fixed #1234

--- frontend/src/components/notes/pages/CreateNote.jsx
+++ frontend/src/components/notes/pages/CreateNote.jsx
@@ -1,5 +1,29 @@
 function CreateNote() {
-  return <div>CreateNote</div>;
+  return (
+    <form
+      action=""
+      className="flex flex-col justify-center items-center h-full gap-5"
+    >
+      <input
+        required
+        type="text"
+        placeholder="Note Title"
+        className="input input-bordered w-full max-w-lg"
+      />
+      <textarea
+        required
+        className="textarea textarea-bordered w-full max-w-lg"
+        placeholder="Details"
+      ></textarea>
+      <select className="select select-bordered w-full max-w-lg">
+        <option disabled selected>
+          Select Note Category
+        </option>
+        <option>Han Solo</option>
+        <option>Greedo</option>
+      </select>
+    </form>
+  );
 }
 
 export default CreateNote;
