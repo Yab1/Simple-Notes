064e7f0afcc4f9562fdd9101c272b5df28c1e1da
Yab1 <yabilisanu@gmail.com>
2023-12-17 12:21:30 +0300

    Refactor Notes component to use React Router Link - Replaced button with Link component for better navigation. - Improves user experience by enabling seamless navigation via React Router Link. (#123)

--- frontend/src/components/notes/pages/Notes.jsx
+++ frontend/src/components/notes/pages/Notes.jsx
@@ -1,23 +1,28 @@
+import { Link } from "react-router-dom";
+
 function Notes() {
   return (
-    <div className="card w-1/2 bg-pink-100 shadow-xl">
-      <div className="card-body grid grid-cols-4">
-        <h2 className="card-title col-span-2">Financial Tips </h2>
-        <div className="card-actions justify-end col-start-4">
-          <button className="btn btn-sm btn-info">Assignment</button>
+    <Link to={"/edit/1"}>
+      <div className="card w-1/2 bg-pink-100 shadow-xl cursor-pointer">
+        <div className="card-body grid grid-cols-4">
+          <h2 className="card-title col-span-2">Financial Tips </h2>
+          <div className="card-actions justify-end col-start-4">
+            <div className="badge badge-info py-5 rounded-lg font-bold text-white">
+              Assignment
+            </div>
+          </div>
+          <p className="text-sm py-2 col-span-full">
+            Learn essential financial tips for managing your budget and
+            achieving your savings goals. Discover strategies to make the most
+            of your hard-earned money.
+          </p>
+          <p className="uppercase">09:35PM</p>
+          <p className="uppercase col-span-2 col-start-3 ml-auto">
+            07 January 2023
+          </p>
         </div>
-
-        <p className="text-sm py-2 col-span-full">
-          Learn essential financial tips for managing your budget and achieving
-          your savings goals. Discover strategies to make the most of your
-          hard-earned money.
-        </p>
-        <p className="uppercase">09:35PM</p>
-        <p className="uppercase col-span-2 col-start-3 ml-auto">
-          07 January 2023
-        </p>
       </div>
-    </div>
+    </Link>
   );
 }
 
