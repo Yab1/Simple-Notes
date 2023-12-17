60720b77f7028a12e4fa3e6103d2966794c244f5
Yab1 <yabilisanu@gmail.com>
2023-12-17 08:48:55 +0300

    Refactor path aliases for specific component directories
    Define path aliases for specific component directories to enhance code
    readability and maintainability. By creating aliases for "@/notes" and
    "@/auth", it becomes easier to locate and differentiate these components
    within the project structure. This change simplifies navigation and
    collaboration for developers. No related issues.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src/"),
+      "@/notes": path.resolve(__dirname, "./src/components/notes/"),
+      "@/auth": path.resolve(__dirname, "./src/components/auth/"),
     },
   },
 });
