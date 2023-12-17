460b8037c1ec016a46767319378779b04fd97ba3
Yab1 <yabilisanu@gmail.com>
2023-12-17 14:47:14 +0300

    Refactor CreateNote form and add form validation
    Refactored the CreateNote form to use useFormik for form handling and
    added form validation to improve user input experience. Replaced
    hard-coded options for tags with dynamic options from Redux store.
    Improved UI by displaying validation error messages. No associated
    issues.

--- frontend/src/components/notes/pages/CreateNote.jsx
+++ frontend/src/components/notes/pages/CreateNote.jsx
@@ -1,27 +1,90 @@
+import { useSelector } from "react-redux";
+import { useFormik } from "formik";
+import { noteSchema } from "../schema";
+
 function CreateNote() {
+  const tags = useSelector((state) => state.ui.tags);
+  // const dispatch = useDispatch();
+  // console.log(dispatch);
+
+  const renderTags = tags.map((tag, index) => (
+    <option key={index} value={tag.name}>
+      {tag.name}
+    </option>
+  ));
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      detail: "",
+      tag: "",
+    },
+    validationSchema: noteSchema,
+    validateOnChange: false,
+    validateOnBlur: false,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleSelectChange = (event) => {
+    formik.setFieldValue("tag", event.target.value);
+  };
+
   return (
-    <form
-      action=""
-      className="flex flex-col justify-center items-center h-full gap-5"
-    >
+    <form onSubmit={formik.handleSubmit} className="flex flex-col w-[420px]">
       <input
-        required
+        id="title"
         type="text"
         placeholder="Note Title"
-        className="input input-bordered w-full max-w-lg"
+        className="input input-bordered"
+        value={formik.values.title}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
+      {formik.errors.title && (
+        <p className="text-red-500 text-sm self-start ml-3 mt-px">
+          {formik.errors.title}
+        </p>
+      )}
+
       <textarea
-        required
-        className="textarea textarea-bordered w-full max-w-lg"
-        placeholder="Details"
+        type="text"
+        id="detail"
+        placeholder="detail"
+        className="textarea textarea-bordered mt-3"
+        value={formik.values.detail}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       ></textarea>
-      <select className="select select-bordered w-full max-w-lg">
-        <option disabled selected>
-          Select Note Category
+      {formik.errors.detail && (
+        <p className="text-red-500 text-sm self-start ml-3 mt-px">
+          {formik.errors.detail}
+        </p>
+      )}
+
+      <select
+        id="tag"
+        name="tag"
+        className="select select-bordered w-full max-w-lg mt-3"
+        value={formik.values.tag}
+        onChange={handleSelectChange}
+        onBlur={formik.handleBlur}
+      >
+        <option value="Choose a tag" className="text-gray-400">
+          Chooe a tag
         </option>
-        <option>Han Solo</option>
-        <option>Greedo</option>
+        {renderTags}
       </select>
+      {formik.touched.tag && formik.errors.tag && (
+        <p className="text-red-500 text-sm self-start ml-3 mt-px">
+          {formik.errors.tag}
+        </p>
+      )}
+
+      <button type="submit" className="btn bg-blue-300 px-7 mt-5">
+        Add Note
+      </button>
     </form>
   );
 }
