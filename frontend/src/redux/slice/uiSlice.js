2f8d469ef177842f54a2344ecc8b13b77e874c32
Yab1 <yabilisanu@gmail.com>
2023-12-18 21:08:28 +0300

    Refactor UI initial state to include additional categories
    Updated the initial state in uiSlice.js to feature more task categories
    for improved task organization. This change enhances the user experience
    by offering a wider range of customization options. No related issues
    are affected.

--- frontend/src/redux/slice/uiSlice.js
+++ frontend/src/redux/slice/uiSlice.js
@@ -11,6 +11,16 @@ const initialState = {
     { name: "Study", background: "bg-error", badge: "badge-error" },
     { name: "Wishlist", background: "bg-success", badge: "badge-success" },
     { name: "Work", background: "bg-primary", badge: "badge-primary" },
+
+    { name: "Assignment", background: "bg-info", badge: "badge-info" },
+    { name: "Ideas", background: "bg-success", badge: "badge-success" },
+    { name: "Meetings", background: "bg-primary", badge: "badge-primary" },
+    { name: "Personal", background: "bg-secondary", badge: "badge-secondary" },
+    { name: "Project", background: "bg-warning", badge: "badge-warning" },
+    { name: "Shopping", background: "bg-info", badge: "badge-info" },
+    { name: "Study", background: "bg-error", badge: "badge-error" },
+    { name: "Wishlist", background: "bg-success", badge: "badge-success" },
+    { name: "Work", background: "bg-primary", badge: "badge-primary" },
   ],
 };
 
