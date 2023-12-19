566b3cf7cc8192572bb6a9eed144e85867a61ab5
Yab1 <yabilisanu@gmail.com>
2023-12-19 20:37:02 +0300

    Create color mapping constants for event categories
    Introduce a new file defining color mappings for various event
    categories, enhancing consistency and maintainability of the codebase.
    This centralization will facilitate easier updates and ensure visual
    coherence across the application. No associated issues.

---
+++ frontend/src/constants/colorMapping.js
@@ -0,0 +1,40 @@
+const colorMapping = Object.freeze({
+  ASSIGNMENT: {
+    textColor: "text-rose-600",
+    bgColor: "bg-rose-600",
+  },
+  IDEAS: {
+    textColor: "text-pink-600",
+    bgColor: "bg-pink-600",
+  },
+  MEETINGS: {
+    textColor: "text-fuchsia-600",
+    bgColor: "bg-fuchsia-600",
+  },
+  PERSONAL: {
+    textColor: "text-amber-600",
+    bgColor: "bg-amber-600",
+  },
+  PROJECT: {
+    textColor: "text-emerald-600",
+    bgColor: "bg-emerald-600",
+  },
+  SHOPPING: {
+    textColor: "text-sky-600",
+    bgColor: "bg-sky-600",
+  },
+  STUDY: {
+    textColor: "text-lime-600",
+    bgColor: "bg-lime-600",
+  },
+  WISHLIST: {
+    textColor: "text-violet-600",
+    bgColor: "bg-violet-600",
+  },
+  WORK: {
+    textColor: "text-purple-700",
+    bgColor: "bg-purple-700",
+  },
+});
+
+export default colorMapping;
