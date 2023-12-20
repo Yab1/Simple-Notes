2124195169c6681231af695314faedd8428c4322
Yab1 <yabilisanu@gmail.com>
2023-12-20 19:16:23 +0300

    Refactor Navbar styling for improved visual consistency
    Modify Navbar styling to enhance visual consistency and update 'Add New
    Note' button appearance.

--- frontend/src/components/notes/widgets/Navbar.jsx
+++ frontend/src/components/notes/widgets/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
   const location = useLocation();
 
   return (
-    <header className="navbar bg-base-300 ">
+    <header className="navbar bg-base">
       <div className="dropdown dropdown-bottom navbar-start w-72 flex gap-3">
         <Avater />
       </div>
@@ -25,7 +25,10 @@ function Navbar() {
       <div className="navbar-end flex-1 px-3">
         <ThemeController />
 
-        <Link to={`create`} className="btn bg-blue-300 ml-5">
+        <Link
+          to={`create`}
+          className="btn bg-blue-600 hover:border-blue-600 hover:border hover:text-blue-600 ml-5"
+        >
           Add New Note
         </Link>
       </div>
