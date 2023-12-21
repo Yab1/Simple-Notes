472da401724e540caa5a4f18ff3f5694a9a8062f
Yab1 <yabilisanu@gmail.com>
2023-12-21 18:41:09 +0300

    feat: add API proxy configuration for local development
    Enable proxy configuration for the "/api" routes to redirect requests to
    http://localhost:4000 during development, ensuring seamless interaction
    with the backend server. This enhances the local development experience
    by facilitating API testing without CORS issues.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,15 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src/"),
