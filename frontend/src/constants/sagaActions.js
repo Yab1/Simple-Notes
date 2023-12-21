f462686e09b1bb78451610184a54f06747248dae
Yab1 <yabilisanu@gmail.com>
2023-12-21 18:41:55 +0300

    Add saga actions for note CRUD operations
    This commit adds new saga action constants for handling note CRUD
    operations, providing a standardized way to manage note-related
    asynchronous tasks within the application.
    
    Issue #123

---
+++ frontend/src/constants/sagaActions.js
@@ -0,0 +1,19 @@
+const SagaActions = Object.freeze({
+  // AUTH_WITH_GOOGLE = "auth/with-google",
+  // LOG_OUT = "auth/log-out",
+  // INITIALIZE_USER = "auth/initialize-user",
+
+  // // USER CRUD
+  // CREATE_USER = "firestore/create-user",
+  // READ_USER = "firestore/read-user",
+  // UPDATE_USER = "firestore/update-user",
+  // DELETE_USER = "firestore/delete-user",
+
+  // PROJECT CRUD
+  CREATE_NOTE: "create-note",
+  READ_NOTES: "read-notes",
+  UPDATE_NOTE: "update-note",
+  DELETE_NOTE: "delete-note",
+});
+
+export default SagaActions;
