d32995b0773130c575a6d25524731cce471976f1
Yab1 <yabilisanu@gmail.com>
2023-12-21 18:44:47 +0300

    Refactor Home component to dispatch READ_NOTES action on mount
    Add useEffect hook to dispatch READ_NOTES action when Home component
    mounts, enabling retrieval of notes upon page load. This enhances user
    experience by ensuring immediate access to the latest notes. No
    associated issues.

--- frontend/src/layouts/Home.jsx
+++ frontend/src/layouts/Home.jsx
@@ -1,7 +1,15 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { Navbar, Drawer } from "@/components/notes/widgets";
+import { sagaActions } from "@/constants";
+import { useDispatch } from "react-redux";
 
 function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch({ type: sagaActions.READ_NOTES });
+  }, [dispatch]);
+
   return (
     <Fragment>
       <Navbar />
