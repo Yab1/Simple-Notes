f09f35335f392901ba4b390fe2938189ba5f8b5e
Yab1 <yabilisanu@gmail.com>
2023-12-22 14:47:41 +0300

    Add delete note saga for handling note deletion
    This commit introduces a new saga to handle the deletion of notes. Upon
    receiving a delete note request, the saga sends a DELETE request to the
    API and dispatches actions based on the response. In case of failure, an
    error message is dispatched. This functionality enhances the application
    by enabling seamless handling of note deletions.
    
    No references.

---
+++ frontend/src/redux/saga/crudSagas/deleteNote.js
@@ -0,0 +1,31 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import { dbStart, noteDeleted, dbFailure } from "@/redux/slices";
+import { sagaActions } from "@/constants";
+
+function* deleteNoteSaga({ noteId }) {
+  try {
+    yield put(dbStart());
+
+    const response = yield call(
+      fetch,
+      import.meta.env.VITE_FETCH_API_TARGET + "/" + noteId,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = yield call([response, "json"]);
+
+    if (!response.ok) throw Error(json.error);
+
+    yield put(noteDeleted(json._id));
+  } catch (error) {
+    console.log(error);
+    yield put(dbFailure(error.message));
+  }
+}
+
+function* watchDeleteNoteRequest() {
+  yield takeLatest(sagaActions.DELETE_NOTE, deleteNoteSaga);
+}
+
+export default watchDeleteNoteRequest;
