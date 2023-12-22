04d375e382aead6b19b0c64710244638e1346484
Yab1 <yabilisanu@gmail.com>
2023-12-22 12:20:21 +0300

    Refactor note editing page and adjust page imports
    Replace the EditNote component with NoteDetails for functionality
    enhancement and update related imports accordingly.
    
    Resolves: #123

--- frontend/src/components/notes/pages/index.js
+++ frontend/src/components/notes/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Notes } from "./Notes";
 export { default as CreateNote } from "./CreateNote";
-export { default as EditNote } from "./EditNote";
+export { default as NoteDetails } from "./noteDetails/NoteDetails";
 export { default as Profile } from "./Profile";
 export { default as Settings } from "./Settings";
 export { default as About } from "./About";
