cca7aa5547a7d4d4b63322c97167ec450dd1ed82
Yab1 <yabilisanu@gmail.com>
2023-12-22 13:27:22 +0300

    Refactor note details view mode UI for improved clarity and functionality
    Refactored the UI to include tooltip buttons for deleting and editing
    notes, enhancing user interaction and providing clearer navigation. This
    change improves the overall user experience for managing notes in view
    mode.
    
    No GitHub issue is associated with these changes.

--- frontend/src/components/notes/pages/noteDetails/components/ViewMode.jsx
+++ frontend/src/components/notes/pages/noteDetails/components/ViewMode.jsx
@@ -11,31 +11,56 @@ function ViewMode() {
 
   return (
     <div className="px-5 flex flex-col gap-5 mt-14">
-      <div className="flex justify-between items-center mb-2">
+      <div className="flex items-center mb-2 gap-3">
         <div
           className={`badge h-10 rounded-md ${
             colorMapping[note.tag.toUpperCase()].bgColor
-          } border-none text-white `}
+          } border-none text-white font-semibold px-3`}
         >
           {note.tag}
         </div>
-        <button
-          className="btn btn-circle"
-          onClick={() => dispatch(toggleEditing())}
+        <div
+          className="tooltip tooltip-bottom ml-auto "
+          data-tip="
+        Delete note"
         >
-          <svg
-            className="w-5 aspect-square text-gray-800 dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 21 21"
+          <button className="btn btn-circle">
+            <svg
+              className="w-5 aspect-square text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 18 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.3"
+                d="M1 5h16M7 8v8m4-8v8M7 1h4a1 1 0 0 1 1 1v3H6V2a1 1 0 0 1 1-1ZM3 5h12v13a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5Z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="tooltip tooltip-bottom" data-tip="Edit note">
+          <button
+            className="btn btn-circle"
+            onClick={() => dispatch(toggleEditing())}
           >
-            <path
-              stroke="currentColor"
-              d="M7.418 17.861 1 20l2.139-6.418m4.279 4.279 10.7-10.7a3.027 3.027 0 0 0-2.14-5.165c-.802 0-1.571.319-2.139.886l-10.7 10.7m4.279 4.279-4.279-4.279m2.139 2.14 7.844-7.844m-1.426-2.853 4.279 4.279"
-            />
-          </svg>
-        </button>
+            <svg
+              className="w-5 aspect-square text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 21 21"
+            >
+              <path
+                stroke="currentColor"
+                d="M7.418 17.861 1 20l2.139-6.418m4.279 4.279 10.7-10.7a3.027 3.027 0 0 0-2.14-5.165c-.802 0-1.571.319-2.139.886l-10.7 10.7m4.279 4.279-4.279-4.279m2.139 2.14 7.844-7.844m-1.426-2.853 4.279 4.279"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
 
       <h2 className="card-title col-span-2 text-4xl">{note.title}</h2>
