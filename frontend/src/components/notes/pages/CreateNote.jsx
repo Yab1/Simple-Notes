b89ef1c285d7bdca4d59618e627d880f8665f0d8
Yab1 <yabilisanu@gmail.com>
2023-12-22 13:26:59 +0300

    Refactor note creation form for improved layout and error handling
    Reorganize form elements and apply error styling for a better user
    experience. Improve layout and error indication while adding notes.
    Issue #123

--- frontend/src/components/notes/pages/CreateNote.jsx
+++ frontend/src/components/notes/pages/CreateNote.jsx
@@ -31,64 +31,80 @@ function CreateNote() {
   };
 
   return (
-    <form onSubmit={formik.handleSubmit} className="flex flex-col w-[420px]">
-      <input
-        id="title"
-        type="text"
-        placeholder="Title"
-        className="input input-bordered"
-        value={formik.values.title}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      />
-      {formik.errors.title && (
-        <p className="text-red-500 text-sm self-start ml-3 mt-px">
-          {formik.errors.title}
-        </p>
-      )}
-
-      <textarea
-        type="text"
-        id="detail"
-        rows="6"
-        placeholder="Detail"
-        className="textarea textarea-bordered mt-3"
-        value={formik.values.detail}
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-      ></textarea>
-      {formik.errors.detail && (
-        <p className="text-red-500 text-sm self-start ml-3 mt-px">
-          {formik.errors.detail}
-        </p>
-      )}
-
-      <select
-        id="tag"
-        name="tag"
-        className="select select-bordered w-full max-w-lg mt-3"
-        value={formik.values.tag}
-        onChange={handleSelectChange}
-        onBlur={formik.handleBlur}
+    <div className="flex justify-center items-center h-full">
+      <form
+        onSubmit={formik.handleSubmit}
+        className="grid grid-cols-3 grid-rows-6 gap-3 mt-5"
       >
-        <option value="Choose a tag" className="text-gray-400">
-          Chooe a tag
-        </option>
-        {renderTags}
-      </select>
-      {formik.touched.tag && formik.errors.tag && (
-        <p className="text-red-500 text-sm self-start ml-3 mt-px">
-          {formik.errors.tag}
-        </p>
-      )}
+        <div>
+          <input
+            id="title"
+            type="text"
+            placeholder="Title"
+            className={`input input-bordered w-full ${
+              formik.errors.title ? "input-error" : ""
+            }`}
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {formik.errors.title && (
+            <p className="text-red-500 text-sm self-start ml-3 mt-px">
+              {formik.errors.title}
+            </p>
+          )}
+        </div>
 
-      <button
-        type="submit"
-        className="btn text-white bg-blue-600 hover:border-blue-600 hover:border hover:text-blue-600 px-7 mt-5"
-      >
-        Add Note
-      </button>
-    </form>
+        <div className="col-start-2 col-span-2 row-span-full flex flex-col">
+          <textarea
+            type="text"
+            id="detail"
+            rows="10"
+            placeholder="Detail"
+            className={`textarea textarea-bordered h-full ${
+              formik.errors.detail ? "textarea-error" : ""
+            }`}
+            value={formik.values.detail}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          ></textarea>
+          {formik.errors.detail && (
+            <p className="text-red-500 text-sm ml-3 mt-px">
+              {formik.errors.detail}
+            </p>
+          )}
+        </div>
+        <div className="col-start-1 row-start-2">
+          <select
+            id="tag"
+            name="tag"
+            className={`select select-bordered max-w-lg w-full ${
+              formik.errors.tag ? "select-error" : ""
+            }`}
+            value={formik.values.tag}
+            onChange={handleSelectChange}
+            onBlur={formik.handleBlur}
+          >
+            <option value="Choose a tag" className="text-gray-400">
+              Chooe a tag
+            </option>
+            {renderTags}
+          </select>
+          {formik.touched.tag && formik.errors.tag && (
+            <p className="text-red-500 text-sm self-start ml-3 mt-px">
+              {formik.errors.tag}
+            </p>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className="btn text-white bg-blue-600 hover:border-blue-600 hover:border hover:text-blue-600 px-7 col-start-1 row-start-3"
+        >
+          Add Note
+        </button>
+      </form>
+    </div>
   );
 }
 
