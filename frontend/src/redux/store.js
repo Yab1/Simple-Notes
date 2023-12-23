f069c43ef9960748e15c7754549ae571c5c5760f
Yab1 <yabilisanu@gmail.com>
2023-12-23 18:07:25 +0300

    Add authReducer to store Add authReducer to store in order to handle authentication states and actions effectively, enhancing security and user management. No related issues.

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import logger from "redux-logger";
-import { uiReducer, notesReducer } from "./slices";
+import { uiReducer, notesReducer, authReducer } from "./slices";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
@@ -9,6 +9,7 @@ const sagaMiddleware = createSagaMiddleware();
 const store = configureStore({
   reducer: {
     ui: uiReducer,
+    auth: authReducer,
     notes: notesReducer,
   },
   middleware: (getDefaultMiddleware) =>
