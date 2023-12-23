3f24bab042b869dc32e08b98b2d276a9487db494
Yab1 <yabilisanu@gmail.com>
2023-12-23 21:25:32 +0300

    Refactor router logic and handle authentication status - Use useSelector to access auth status and dynamically set router - Render loading component based on auth status - Improve handling of authentication flow and UI - Address authentication-related user experience - Close #123

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import { Fragment } from "react";
 import { RouterProvider } from "react-router-dom";
-import { router, authRouter } from "@/router";
+import { useSelector } from "react-redux";
+import { Loading } from "@/layouts";
+import { status } from "@/constants";
+import { getRouter } from "@/router";
 
 function App() {
-  const auth = false;
-  return <RouterProvider router={auth ? router : authRouter} />;
+  const authStatus = useSelector((state) => state.auth.authStatus);
+  const router = getRouter(Boolean(authStatus === status.SUCCEEDED));
+
+  return (
+    <Fragment>
+      {authStatus === status.LOADING ? <Loading /> : null}
+      <RouterProvider router={router} />;
+    </Fragment>
+  );
 }
 
 export default App;
