77792504d9d6857cce5e590a28c129d75793e7c9
Yab1 <yabilisanu@gmail.com>
2023-12-23 22:02:49 +0300

    Refactor signup saga to handle API response and set user data
    Handle API response in signup saga, set user data in local storage, and
    dispatch success action. Improve user experience and data management for
    signup. Closes #123.

--- frontend/src/redux/saga/authSagas/signup.js
+++ frontend/src/redux/saga/authSagas/signup.js
@@ -6,7 +6,27 @@ function* signupSaga({ credentials }) {
   try {
     yield put(authStart());
 
-    console.log(credentials);
+    const response = yield call(
+      fetch,
+      import.meta.env.VITE_API_USER_SIGNUP_ENDPOINT,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      }
+    );
+
+    const json = yield call([response, "json"]);
+
+    if (!response.ok) throw Error(json.error);
+
+    localStorage.setItem("daisy-notes-user", JSON.stringify(json));
+    yield put(
+      authSucceeded({
+        json,
+        msg: "Your account has been created successfully. Thank you for joining us.",
+      })
+    );
   } catch (error) {
     yield put(authFailure(error.message));
   }
