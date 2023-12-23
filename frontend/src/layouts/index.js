8fcf0ffebe866c45ea55c58651d0257dd95d56a7
Yab1 <yabilisanu@gmail.com>
2023-12-23 21:27:14 +0300

    Add Loading component to index layout
    Include the Loading component in the index layout to enable its use
    across the application for indicating loading state. This enhances user
    experience by providing visual feedback during data fetching operations.
    No related issues.

--- frontend/src/layouts/index.js
+++ frontend/src/layouts/index.js
@@ -1,3 +1,4 @@
 export { default as Auth } from "./Auth";
 export { default as Home } from "./Home";
 export { default as ErrorPage } from "./ErrorPage";
+export { default as Loading } from "./Loading";
