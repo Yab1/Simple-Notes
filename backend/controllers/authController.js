db1c71a42a41b07e899ed383e8266cafb09263e9
Yab1 <yabilisanu@gmail.com>
2023-12-23 14:50:13 +0300

    Refactor sign-up endpoint to store user data
    Update sign-up endpoint to store user details in the database for
    improved user management and data persistence. This change enhances the
    user experience by enabling proper user data management.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,9 @@ import Auth from "../models/authModel.js";
 // Sign up user
 const signUp = async (req, res) => {
   try {
-    res.json({ mssg: "SignUp User" });
+    const { username, email, password, profile } = req.body;
+    const user = await Auth.signup(username, email, password, profile);
+    res.status(200).json({ email, user });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
