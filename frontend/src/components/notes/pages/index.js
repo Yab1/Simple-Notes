e0bf0766d64843bf1e5502f1ada3ccbfd3699741
Yab1 <yabilisanu@gmail.com>
2023-12-23 21:24:50 +0300

    Refactor router and page routes
    Remove unused child routes from auth and notes pages and corresponding
    imports. Update the router to exclude the unused child routes. This
    cleanup streamlines the codebase and eliminates unnecessary components.
    Fixes #123

--- frontend/src/components/notes/pages/index.js
+++ frontend/src/components/notes/pages/index.js
@@ -4,4 +4,3 @@ export { default as NoteDetails } from "./noteDetails/NoteDetails";
 export { default as Profile } from "./Profile";
 export { default as Settings } from "./Settings";
 export { default as About } from "./About";
-export { default as childRoutes } from "./routes";
