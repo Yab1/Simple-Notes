b7e52e59af9e7ed8ad2cc7425b88c5a864a721a5
Yab1 <yabilisanu@gmail.com>
2023-12-23 18:07:33 +0300

    Create authSlice for authentication status and actions
    Introduce a new authSlice to manage authentication status and related
    actions, enabling the handling of authentication states with ease. This
    facilitates clear and organized management of authentication-related
    logic throughout the application. Add the authReducer to the index for
    export. No associated issues.

---
+++ frontend/src/redux/slices/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { status } from "@/constants";
+
+const initialState = {
+  authStatus: status.IDLE,
+  authError: "",
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    authIdle: () => initialState,
+    authStart: (state) => {
+      state.authStatus = status.LOADING;
+    },
+    authSucceeded: (state, action) => {
+      state.authStatus = status.SUCCEEDED;
+      state.user = action.payload;
+    },
+    authFailure: (state, action) => {
+      state.authStatus = status.FAILED;
+      state.authError = action.payload;
+    },
+  },
+});
+
+export const { authIdle, authStart, authSucceeded, authFailure } =
+  authSlice.actions;
+export default authSlice.reducer;
--- frontend/src/redux/slices/index.js
+++ frontend/src/redux/slices/index.js
@@ -2,3 +2,5 @@ export { default as uiReducer } from "./uiSlice";
 export * from "./uiSlice";
 export { default as notesReducer } from "./notesSlice";
 export * from "./notesSlice";
+export { default as authReducer } from "./authSlice";
+export * from "./authSlice";
