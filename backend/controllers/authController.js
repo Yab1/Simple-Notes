62bb93023d7ca32cc2094d628cc8359c4668c31b
Yab1 <yabilisanu@gmail.com>
2023-12-23 17:51:31 +0300

    Refactor authController to create and return JWT token
    Add functionality to create and return a JWT token upon user signup for
    secure authentication. This enhances security and provides a token for
    subsequent user authentication. No related issues.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,20 @@
 import Auth from "../models/authModel.js";
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // Sign up user
 const signUp = async (req, res) => {
   try {
     const { username, email, password, profile } = req.body;
     const user = await Auth.signup(username, email, password, profile);
-    res.status(200).json({ email, user });
+
+    // create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
