d18767d2a849e36b7bcc9fe9f51345c0b184da95
Yab1 <yabilisanu@gmail.com>
2023-12-24 20:09:57 +0300

    Refactor Drawer component to conditionally render loading skeleton
    Adjust Drawer component to conditionally render loading skeleton based
    on the database status. This change provides visual feedback to users
    while data is fetching. Fix #123

--- frontend/src/components/notes/widgets/Drawer.jsx
+++ frontend/src/components/notes/widgets/Drawer.jsx
@@ -1,17 +1,22 @@
 import { Fragment } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import { colorMapping } from "@/constants";
 import { useSelector } from "react-redux";
+import { status } from "@/constants";
+import DrawerSkeleton from "./DrawerSkeleton";
 
 function Drawer() {
-  const tags = useSelector((state) => state.notes.tags);
+  const { tags, dbStatus } = useSelector((state) => state.notes);
 
   const renderTags =
     Object.keys(tags).length > 0
       ? Object.entries(tags).map(([key, value]) => (
           <Fragment key={key}>
             <li className="mx-2">
-              <a className="flex items-center text-white font-medium">
+              <Link
+                to={`/notes/${key}`}
+                className="flex items-center text-white font-medium"
+              >
                 <div
                   className={`w-3 aspect-square ${
                     colorMapping[key.toUpperCase()].bgColor
@@ -25,7 +30,7 @@ function Drawer() {
                 >
                   {value}
                 </div>
-              </a>
+              </Link>
             </li>
             <div className="divider my-1"></div>
           </Fragment>
@@ -35,17 +40,17 @@ function Drawer() {
   return (
     <div className="drawer lg:drawer-open ">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content p-5 h-main overflow-auto">
+      <div className="drawer-content h-main overflow-auto">
         <Outlet />
       </div>
-      <div className="drawer-side h-fit">
+      <div className="drawer-side h-main">
         <label
           htmlFor="my-drawer-2"
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu py-4 px-0 w-72 text-base-content border-r border-r-base-300 grid">
-          {renderTags}
+        <ul className="menu py-4 px-0 w-72 text-base-content border-r border-r-base-300 flex h-full">
+          {dbStatus === status.LOADING ? <DrawerSkeleton /> : renderTags}
         </ul>
       </div>
     </div>
