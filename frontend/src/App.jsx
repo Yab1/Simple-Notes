be0ff0e5e826c5ae28082b3fc376f4f1045d9896
Yab1 <yabilisanu@gmail.com>
2023-12-24 12:30:27 +0300

    Refactor App component to dispatch authSucceeded on user retrieval
    Replace direct authStatus check with useEffect and dispatch
    authSucceeded on user retrieval. This allows for a more modular and
    understandable approach to managing user authentication. No issue
    references.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
-import { Fragment } from "react";
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Loading } from "@/layouts";
+import { useDispatch, useSelector } from "react-redux";
 import { status } from "@/constants";
 import { getRouter } from "@/router";
+import { authSucceeded } from "@/redux/slices/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const authStatus = useSelector((state) => state.auth.authStatus);
   const router = getRouter(Boolean(authStatus === status.SUCCEEDED));
 
-  return (
-    <Fragment>
-      {authStatus === status.LOADING ? <Loading /> : null}
-      <RouterProvider router={router} />
-    </Fragment>
-  );
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("daisy-notes-user"));
+    if (user) dispatch(authSucceeded(user));
+  }, [dispatch]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
