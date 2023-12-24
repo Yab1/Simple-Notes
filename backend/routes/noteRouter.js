ef9a9d5204938ac2f53d86ddfd3452f564799f02
Yab1 <yabilisanu@gmail.com>
2023-12-24 13:56:51 +0300

    Add requireAuth middleware to all note routes
    Utilize requireAuth middleware to enforce authentication for all note
    routes, enhancing overall security and access control.
    #Issue 123

--- backend/routes/noteRouter.js
+++ backend/routes/noteRouter.js
@@ -6,8 +6,12 @@ import {
   deleteNote,
   updateNote,
 } from "../controllers/noteController.js";
+import requireAuth from "../middleware/requireAuth.js";
 
 const noteRouter = Router();
+
+// Require auth for all note routes
+noteRouter.use(requireAuth);
 
 // GET all notes
 noteRouter.get("/", getNotes);
