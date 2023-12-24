47f01bafdae0bd4952e4556fc3e78e063b7d2200
Yab1 <yabilisanu@gmail.com>
2023-12-24 18:21:23 +0300

    Refactor Home component to dynamically set tags
    Modify Home component to update tags based on notes, enhancing user
    experience by providing more relevant and dynamic content. This change
    ensures that tags reflect the actual notes, improving navigation and
    content discovery. Closes #123

--- frontend/src/layouts/Home.jsx
+++ frontend/src/layouts/Home.jsx
@@ -1,14 +1,24 @@
 import { Fragment, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navbar, Drawer } from "@/components/notes/widgets";
 import { sagaActions } from "@/constants";
+import { getTags } from "@/functions";
+import { setTags } from "@/redux/slices";
 
 function Home() {
+  const notes = useSelector((state) => state.notes.notes);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch({ type: sagaActions.READ_NOTES });
   }, [dispatch]);
+
+  useEffect(() => {
+    if (notes.length > 0) {
+      const tags = getTags(notes);
+      dispatch(setTags(tags));
+    }
+  }, [dispatch, notes]);
 
   return (
     <Fragment>
