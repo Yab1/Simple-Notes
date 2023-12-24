f3e57fedaa3751a978d1c8fd6dec10a10d5b5743
Yab1 <yabilisanu@gmail.com>
2023-12-24 08:54:46 +0300

    Refactor signin saga to handle API response and store user data
    Refactored signin saga to handle API response and store user data in
    local storage, ensuring a seamless user authentication process. This
    improves user experience by persisting user credentials and handling API
    responses more efficiently. No associated issue.

--- frontend/src/redux/saga/authSagas/signin.js
+++ frontend/src/redux/saga/authSagas/signin.js
@@ -6,7 +6,23 @@ function* signinSaga({ credentials }) {
   try {
     yield put(authStart());
 
-    console.log(credentials);
+    const response = yield call(
+      fetch,
+      import.meta.env.VITE_API_USER_SIGNIN_ENDPOINT,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(credentials),
+      }
+    );
+
+    const json = yield call([response, "json"]);
+    if (!response.ok) throw Error(json.error);
+
+    console.log(json);
+
+    localStorage.setItem("daisy-notes-user", JSON.stringify(json));
+    yield put(authSucceeded(json));
   } catch (error) {
     yield put(authFailure(error.message));
   }
