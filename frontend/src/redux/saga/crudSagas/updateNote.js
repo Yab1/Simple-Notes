d242b0d2c26b5b17e3c61486151982117ba69828
Yab1 <yabilisanu@gmail.com>
2023-12-24 14:16:04 +0300

    Refactor updateNote saga to retrieve token before updating
    Adding token retrieval before updating the note to ensure proper
    authentication. This change enhances security and ensures that the note
    updates are performed with the appropriate user credentials.

--- frontend/src/redux/saga/crudSagas/updateNote.js
+++ frontend/src/redux/saga/crudSagas/updateNote.js
@@ -1,11 +1,15 @@
-import { put, takeLatest } from "redux-saga/effects";
+import { put, takeLatest, select } from "redux-saga/effects";
 import SagaActions from "@/constants/sagaActions";
 import { dbStart, dbFailure } from "@/redux/slices/notesSlice";
+import { getToken } from "@/redux/slices";
 
 function* updateNoteSaga({ noteId, data }) {
   try {
     yield put(dbStart());
 
+    const token = yield select(getToken);
+
+    console.log(token);
     console.log(noteId);
     console.log(data);
   } catch (error) {
