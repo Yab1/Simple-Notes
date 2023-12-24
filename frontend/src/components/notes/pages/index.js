70a8c2ca1662f8cca3203e29405f97ed625abdaf
Yab1 <yabilisanu@gmail.com>
2023-12-24 20:11:52 +0300

    Add FilteredNotes component for improved note filtering and display. This addition enables better organization and presentation of notes based on specific criteria, enhancing the user experience. No related issues.

--- frontend/src/components/notes/pages/index.js
+++ frontend/src/components/notes/pages/index.js
@@ -4,3 +4,4 @@ export { default as NoteDetails } from "./noteDetails/NoteDetails";
 export { default as Profile } from "./Profile";
 export { default as Settings } from "./Settings";
 export { default as About } from "./About";
+export { default as FilteredNotes } from "./FilteredNotes";
