4e27ec95fc77dbcefce186f8e3a7a9d8179e55e9
Yab1 <yabilisanu@gmail.com>
2023-12-24 13:18:51 +0300

    Refactor Drawer component for responsive layout
    Update the Drawer component to ensure responsiveness by adding the
    'overflow-auto' class, enabling smooth scrolling within the drawer
    content. This change addresses the need for improved user experience
    when viewing lengthy notes within the drawer. No associated issues.

--- frontend/src/components/notes/widgets/Drawer.jsx
+++ frontend/src/components/notes/widgets/Drawer.jsx
@@ -25,7 +25,7 @@ function Drawer() {
   return (
     <div className="drawer lg:drawer-open ">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content px-5 py-10">
+      <div className="drawer-content px-5 py-10 h-main overflow-auto">
         <Outlet />
       </div>
       <div className="drawer-side h-fit">
