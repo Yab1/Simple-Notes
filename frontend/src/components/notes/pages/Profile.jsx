5c187775c42de00356e4b5d3bd9e67d31257cc50
Yab1 <yabilisanu@gmail.com>
2023-12-24 12:31:44 +0300

    Refactor Profile component to dynamically render user data
    Use useSelector hook to access user data from the Redux store and
    dynamically render user avatar and username in the Profile component.
    This approach ensures that the component always displays the most
    up-to-date user information. No relevant issue references.

--- frontend/src/components/notes/pages/Profile.jsx
+++ frontend/src/components/notes/pages/Profile.jsx
@@ -1,24 +1,24 @@
+import { useSelector } from "react-redux";
 import { AvatarSelector } from "../widgets";
 
 function Profile() {
+  const user = useSelector((state) => state.auth.user);
+
   return (
     <div className="grid gap-5">
       <form action="" className="grid grid-cols-5 grid-rows-2">
         <div className="avatar row-span-2 col-span-1 place-self-center">
           <div className="w-32 rounded">
-            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            <img src={user.avatar} />
           </div>
         </div>
 
         <input
           type="text"
           placeholder="Username"
-          className="input input-bordered w-full col-span-3"
-        />
-        <input
-          type="text"
-          placeholder="Password"
-          className="input input-bordered w-full row-start-2 col-span-3 col-start-2"
+          value={user.username}
+          onChange={() => console.log("Typing...")}
+          className="input input-bordered w-full col-span-3 row-span-2 place-self-center"
         />
 
         <button
